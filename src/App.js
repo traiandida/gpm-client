89a87116155dd69d1de3c46156a69b831ea0e670
traian.dida <traian.dida@bonarea.com>
2022-05-04 17:54:20 +0200

    app component refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react'
-import FileTree from './components/FileTree'
+import { treeApi } from './api';
+import Tree from './components/Tree';
+import useFetch from './hooks/useFetch';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
-  return (
 
-      <div>
-        <FileTree/>
+   const {response, isLoading, error } = useFetch({
+     api: treeApi,
+     method: "get",
+     url: "/"
+   })
+
+   const render = () => {
+
+    if(isLoading){
+      return <span>Loading...</span>
+    }
+    if(error){
+      return <span>Error fetching data</span>
+    }
+    return <Tree node={response}/>
+   }
+
+  return (
+    <div>
+      {/* <h1 className='text-center'>GPM-Client</h1> */}
+      <div className='container mt-5 p-5 border'>    
+      <h5>File Tree</h5>    
+        {render()}
       </div>
+    </div>
+
+      
       
   )
 }
